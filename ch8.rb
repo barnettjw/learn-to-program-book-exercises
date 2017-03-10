e4be2f3542080c70194ee247ff5e46d3ae0c8275
James Barnett <barnettjw@gmail.com>
2017-03-09 22:16:52 -0500

    Table of contents

--- ch8.rb
+++ ch8.rb
@@ -1,4 +1,5 @@
 # Building and sorting an array
+
 list = []
 
 while true
@@ -10,3 +11,19 @@ while true
 end
 
 puts list.sort
+
+# Table of contents
+
+line_width = 60
+puts "Table of Contents".center(line_width)
+puts
+
+t_of_c = [["Chapter 1:  ", "Getting Started", "page 1"], 
+          ["Chapter 2:  ", "Numbers", "page 9"],
+          ["Chapter 3:  ", "Letters", "page 13"]]
+
+t_of_c.each do | ch |
+  center = (line_width - ch[0].length) / 2
+  puts ch[0] + ch[1].ljust(center) + ch[2].rjust(center)
+end
+puts
