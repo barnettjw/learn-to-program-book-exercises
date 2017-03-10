45c368dfc1ec787f491268b00091ecd941ba2bbb
James Barnett <barnettjw@gmail.com>
2017-03-09 19:56:24 -0500

    building and sorting an array - cleaned up formatting

--- ch8.rb
+++ ch8.rb
@@ -1,10 +1,12 @@
 # Building and sorting an array
 list = []
+
 while true
   input = gets
   if input.length == 1
     break
   end
-  list.push(input.chomp.downcase)
+  list.push input.chomp.downcase
 end
+
 puts list.sort
