cd30dded338b51203841548f9eb2347b67d9e741
James Barnett <barnettjw@gmail.com>
2017-03-10 19:56:57 -0500

    improved ask method - first commit

---
+++ ch9.rb
@@ -0,0 +1,28 @@
+# Improved ask method
+
+def ask question
+  puts question
+  reply = ""
+
+  while true
+    answer = gets.chomp
+
+    if reply == "yes" 
+      return true
+    elsif reply == "no"
+       return false
+    else
+      puts "Please answer 'yes' or 'no'"
+    end
+  end
+  
+end
+
+ask "Do you like cake?"
+ask "Do you like juice?"
+wets_bed = ask "Do you wet the bed?"
+ask "Do you like pasta?"
+ask "Do you like cereal?"
+
+puts "Thanks for your help"
+puts wets_bed
