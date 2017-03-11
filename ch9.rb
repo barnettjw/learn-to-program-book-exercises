f5409f81688bbebb1b4b5d2827ee7fbff8adcd7e
James Barnett <barnettjw@gmail.com>
2017-03-10 22:33:58 -0500

    modern roman numerals - first commit

--- ch9.rb
+++ ch9.rb
@@ -28,24 +28,38 @@ puts wets_bed
 
 # Old School Roman Numerals
 def get_roman_numeral(year_remaining)
-  roman_numeral_list = 
-  [[1000, "M"],
-    [500, "D"],
-    [100, "C"],
-    [50,"L"],
-    [10, "X"],
-    [5,"V"],
-    [1,"I"]]
-
+  roman_numeral_list = [[1000, "M"], [500, "D"], [100, "C"], [50,"L"], [10, "X"], [5,"V"], [1,"I"]]
   roman_numeral = ""
+
   roman_numeral_list.each do |item|
     count = year_remaining / item[0].to_i
     letter = item[1].to_s
     roman_numeral += letter * count
     year_remaining = year_remaining % item[0].to_i
   end
+
   return roman_numeral
 end
 
-puts "Enter a year to convert it to a Roman Numeral:"
+puts "Enter a year to convert it to a (Old School) Roman Numeral:"
+puts get_roman_numeral(gets.chomp.to_i)
+
+# Modern Roman Numberals
+def get_roman_numeral(year_remaining)
+  roman_numeral_list = 
+   [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], 
+   [50,"L"],[40,"XL"], [10, "X"], [9, "IX"], [5,"V"], [4, "IV"], [1,"I"]]
+  roman_numeral = ""
+
+  roman_numeral_list.each do |item|
+    count = year_remaining / item[0].to_i
+    letter = item[1].to_s
+    roman_numeral += letter * count
+    year_remaining = year_remaining % item[0].to_i
+  end
+
+  return roman_numeral
+end
+
+puts "Enter a year to convert it to a (Modern) Roman Numeral:"
 puts get_roman_numeral(gets.chomp.to_i)
